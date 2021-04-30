b9a7d7d225604c90be5368ac8b53bc65159139ea
Hot-Drinks <31969143+Hot-Drinks@users.noreply.github.com>
2021-04-30 16:32:19 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -83,65 +83,75 @@ include 'dbo.php';
     $conn = OpenCon();
     $stmt = "SELECT * FROM article ORDER BY date DESC LIMIT 10";
     //Attempted to perform a username search
-    //$findUser = "SELECT username FROM user WHERE user.userID = article.userID ORDER BY date DESC LIMIT 10";
+    $findUser = "SELECT username FROM user,article WHERE user.userID = article.userID";
 
-    if($result = mysqli_query($conn, $stmt /* && $result2 = mysqli_query($conn, $findUser)*/)){
-        if(mysqli_num_rows($result) > 0){
-            while($row = mysqli_fetch_array($result)){
-              //  $userSearch = mysqli_fetch_array($result2);
-                $link = "article.php/?q=" . $row['articleID'];
-                    echo "<div class = " . "RecentReviewShowsHere". ">";
-                        echo "<table>";
-                            echo "<tr>";
-                                echo "<td rowspan = 4> <img src=\"" . $row['imageURL']. "\" alt=\"Girl in a jacket\"> " ."</td>";
-                                echo "<td>Headline : ". "<a href=" . $link . ">" . $row['articleTitle'] . "</a></th>";
-                            echo "</tr>";
-                            echo "<tr>";
-                                echo "<td> Anime Name : \n" . $row['animeName'] . "</td>";
-                            echo "</tr>";
-                        /*    echo "<tr>";
-                                echo "<td>By : ". $userSearch['username'] . "</a></th>";
-                            echo "</tr>";  */                          
-                            echo "<tr>";
-                                echo "<td colspan = " .  "2" . "> User Rating : " .  $row['starRating'] ."/5 </td>";
-                            echo "</tr>";
-                            echo "<tr>";
-                                echo "<td> Review : " . firstSentence($row['body']) ." <a href=" . $link . ">(See More)"."</a></td>";
-                            echo "</tr>";
-                        echo "</table><br>";
-                    echo "</div>";
-            /*
-                echo "<th>Title/LinkToArticle</th>";
-                echo "<th>UserName</th>";
-                echo "<th>StarRating</th>";
-                echo "<th>AnimeName</th>";
-            echo "</tr>";
-            */
-            //<a href=$link >$row['articleTitle']</a>
-
-
-
-            //"https://api.jikan.moe/v3/anime/"
-            /*echo "<tr>";
-                echo "<td> <img src=\"" . $row['imageURL']. "\" alt=\"Girl in a jacket\"> " ."</td>";
-                echo "<td>" . "<a href=" . $link . ">" . $row['articleTitle'] . "</a> ". "</td>";
-                echo "<td>" . $row['userID'] . "</td>";
-                echo "<td>" . $row['starRating'] . "</td>";
-                echo "<td>" . $row['animeName'] . "</td>";
-            echo "</tr>";*/
-        }
-        
-        // Free result set
-        mysqli_free_result($result);
-    }
-}
-
-    $conn -> close();
-    ?>
+    if($result = mysqli_query($conn, $stmt))
+        {
+            if ($userSearch = mysqli_query($conn, $findUser)) /* && $result2 = mysqli_query($conn, $findUser)))*/
+                {
+                    if(mysqli_num_rows($result) > 0)
+                        {
+                            if(mysqli_num_rows($userSearch) > 0)
+                                {
+                                    while($row = mysqli_fetch_array($result))
+                                        if($userRow = mysqli_fetch_array($userSearch))
+                                            {
+                                                //$userSearch =  mysqli_query($conn, $findUser);
+                                                $link = "article.php/?q=" . $row['articleID'];
+                                                    echo "<div class = " . "RecentReviewShowsHere". ">";
+                                                        echo "<table>";
+                                                            echo "<tr>";
+                                                                echo "<td rowspan = 5> <img src=\"" . $row['imageURL']. "\" alt=\"Girl in a jacket\"> " ."</td>";
+                                                                echo "<td>Headline : ". "<a href=" . $link . ">" . $row['articleTitle'] . "</a></th>";
+                                                            echo "</tr>";
+                                                            echo "<tr>";
+                                                                echo "<td> Anime Name : \n" . $row['animeName'] . "</td>";
+                                                            echo "</tr>";
+                                                            echo "<tr>";
+                                                                echo "<td>By : ". $userRow['username'] . "</td>";
+                                                            echo "</tr>";      
+                                                            echo "<tr>";
+                                                                echo "<td colspan = " .  "2" . "> User Rating : " .  $row['starRating'] ."/5 </td>";
+                                                            echo "</tr>";
+                                                            echo "<tr>";
+                                                                echo "<td> Review : " . firstSentence($row['body']) ." <a href=" . $link . ">(See More)"."</a></td>";
+                                                            echo "</tr>";
+                                                        echo "</table><br>";
+                                                    echo "</div>";
+                                /*
+                                    echo "<th>Title/LinkToArticle</th>";
+                                    echo "<th>UserName</th>";
+                                    echo "<th>StarRating</th>";
+                                    echo "<th>AnimeName</th>";
+                                echo "</tr>";
+                                */
+                                //<a href=$link >$row['articleTitle']</a>
 
 
 
+                                //"https://api.jikan.moe/v3/anime/"
+                                /*echo "<tr>";
+                                    echo "<td> <img src=\"" . $row['imageURL']. "\" alt=\"Girl in a jacket\"> " ."</td>";
+                                    echo "<td>" . "<a href=" . $link . ">" . $row['articleTitle'] . "</a> ". "</td>";
+                                    echo "<td>" . $row['userID'] . "</td>";
+                                    echo "<td>" . $row['starRating'] . "</td>";
+                                    echo "<td>" . $row['animeName'] . "</td>";
+                                echo "</tr>";*/
+                            }
+                            
+                            // Free result set
+                            mysqli_free_result($result);
+                        }
+                    }
+                        }
+                        }
 
-</div>
+                        $conn -> close();
+                        ?>
 
-</body>
+
+
+
+            </div>
+
+            </body>
